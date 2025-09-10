3ff09522c4e4aa5e58500287af031aa443476c96
Kateryna Vichkovska <katya.vichkovska@gmail.com>
2025-09-10 23:29:39 +0300

    add base notes http server

---
+++ index.js
@@ -0,0 +1,103 @@
+import  express  from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+const app = express();
+const port = 3000;
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(
+morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    JSON.stringify(req.body)
+  ].join(' ')
+}))
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const note = notes.find(note => note.id === id);
+
+  if(!note) {
+    return res.status(404).end();
+  }
+
+  res.json(note);
+});
+
+app.post('/api/notes', (req, res) => {
+  const body = req.body
+
+  if (!body.content) {
+    return res.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+  res.json(note)
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  notes = notes.filter(note => note.id !== id);
+  
+  res.status(204).end();
+});
+
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}/`);
+});
